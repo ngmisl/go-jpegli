229a6097079f589451229fcccde44eac688e01f4
chris <ngmi@posteo.es>
2024-04-09 12:24:05 +0200

    added args, memory and typesafety

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # go-jpegli
 
 ![2024-04-08_21-57](https://github.com/ngmisl/go-jpegli/assets/98217124/64611d1c-43a7-44b0-979e-86ed340656f5)
+
+## Usage
+
+`go run main.go 1.png 2.png 3.png`
--- main.go
+++ main.go
@@ -3,55 +3,65 @@ package main
 import (
 	"bytes"
 	"fmt"
-	"image/png" // Import PNG support
+	"image/png"
 	"log"
 	"net/http"
 	"os"
+	"path/filepath"
 
-	"github.com/gen2brain/jpegli" // Import Jpegli package
+	"github.com/gen2brain/jpegli"
 )
 
 // ToJpeg converts a PNG image to Jpegli format
 func ToJpeg(imageBytes []byte) ([]byte, error) {
 	// Detect the content type
 	contentType := http.DetectContentType(imageBytes)
-	switch contentType {
-	case "image/png":
-		// Decode the PNG image bytes
-		img, err := png.Decode(bytes.NewReader(imageBytes))
-		if err != nil {
-			return nil, err
-		}
-
-		// Encode the image as a Jpegli file
-		buf := new(bytes.Buffer)
-		if err := jpegli.Encode(buf, img, nil); err != nil {
-			return nil, err
-		}
-
-		return buf.Bytes(), nil
+	if contentType != "image/png" {
+		return nil, fmt.Errorf("unable to convert %#v to Jpegli", contentType)
 	}
 
-	return nil, fmt.Errorf("unable to convert %#v to Jpegli", contentType)
+	// Decode the PNG image bytes
+	img, err := png.Decode(bytes.NewReader(imageBytes))
+	if err != nil {
+		return nil, err
+	}
+
+	// Encode the image as a Jpegli file
+	buf := new(bytes.Buffer)
+	if err := jpegli.Encode(buf, img, nil); err != nil {
+		return nil, err
+	}
+	return buf.Bytes(), nil
 }
 
 func main() {
-	// Read the input PNG image from a file
-	inputImageBytes, err := os.ReadFile("1.png")
-	if err != nil {
-		log.Fatal("Error reading PNG file:", err)
+	// Check if any command-line arguments are provided
+	if len(os.Args) < 2 {
+		log.Fatal("Usage: go run main.go [PNG_FILE_1] [PNG_FILE_2] ... [PNG_FILE_N]")
 	}
 
-	// Convert PNG to Jpegli
-	convertedImageBytes, err := ToJpeg(inputImageBytes)
-	if err != nil {
-		log.Fatal("Error converting PNG to Jpegli:", err)
-	}
+	for _, arg := range os.Args[1:] {
+		// Read the input PNG image from a file
+		inputImageBytes, err := os.ReadFile(arg)
+		if err != nil {
+			log.Printf("Error reading PNG file %s: %v", arg, err)
+			continue
+		}
 
-	// Write the Jpegli output to a file
-	if err := os.WriteFile("output.jpeg", convertedImageBytes, 0644); err != nil {
-		log.Fatal("Error saving Jpegli file:", err)
-	}
+		// Convert PNG to Jpegli
+		convertedImageBytes, err := ToJpeg(inputImageBytes)
+		if err != nil {
+			log.Printf("Error converting PNG %s to Jpegli: %v", arg, err)
+			continue
+		}
 
-	log.Println("Conversion to Jpegli completed successfully.")
+		// Construct the output file name
+		outputFilename := filepath.Base(arg[:len(arg)-len(filepath.Ext(arg))]) + ".jpeg"
+		if err := os.WriteFile(outputFilename, convertedImageBytes, 0644); err != nil {
+			log.Printf("Error saving Jpegli file %s: %v", outputFilename, err)
+			continue
+		}
+
+		log.Printf("Conversion of %s to Jpegli completed successfully.", arg)
+	}
 }
